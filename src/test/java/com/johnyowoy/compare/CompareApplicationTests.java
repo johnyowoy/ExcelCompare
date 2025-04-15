f5426b293ed0065c976a5df991d8b6257e8598ba
johnyowoy <john-7113113@hotmail.com>
2025-04-15 22:30:25 +0800

    init project

---
+++ src/test/java/com/johnyowoy/compare/CompareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnyowoy.compare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
